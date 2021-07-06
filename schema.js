68624d129e77eafa56e54ed01482ab763de3c039
Abi Scholz <abi.scholz@gmail.com>
2021-07-06 14:55:23 -0400

    First commit: setting up express graphql server

---
+++ schema.js
@@ -0,0 +1,89 @@
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+} = require('graphql');
+
+const beers = [
+  {
+    name: 'Finback IPA',
+    description: 'Classic west coast ipa',
+    brewery: 'Finback',
+  },
+  {
+    name: 'Sweet Action',
+    description: 'easy drinking Brooklyn ipa',
+    brewery: 'Sixpoint',
+  },
+  {
+    name: 'Watermelon Glow-Up',
+    description: 'berliner-weisse style sour ale',
+    brewery: 'Folksbier',
+  },
+];
+
+const breweries = [
+  {
+    name: 'Finback',
+    address: 'Queens',
+  },
+  {
+    name: 'Sixpoint',
+    address: 'Red Hook',
+  },
+  {
+    name: 'Folksbier',
+    address: 'Carrol Gardens',
+  },
+];
+
+const breweryType = new GraphQLObjectType({
+  name: 'brewery',
+  fields: {
+    name: { type: GraphQLString },
+    address: { type: GraphQLString },
+  },
+});
+
+const beerType = new GraphQLObjectType({
+  name: 'beer',
+  fields: {
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    brewery: {
+      type: breweryType,
+      resolve: (source, params) => {
+        return breweries[source.brewery];
+      },
+    },
+  },
+});
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    beer: {
+      type: beerType,
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (source, { id }) => {
+        return beers[id];
+      },
+    },
+    beers: {
+      type: new GraphQLList(beerType),
+      resolve: () => {
+        return beers;
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: queryType,
+});
+
+module.exports = schema;
